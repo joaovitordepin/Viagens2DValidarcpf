eb909edbf35799135c16ac01c5f6ac80c0b9390f
programadormovel <programadormovel@gmail.com>
2023-10-26 11:08:03 -0300

    Aula relâmpago

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Turista mochileiro = new Turista();
-        
+        mochileiro.setNome("Adriano");
+
+        Turista novoTurista = new Turista("Maria");
+        novoTurista.setNome("Meire");
+        if(Util.ValidacaoCPF("123")){
+            novoTurista.setCpf("123");
+        } else{
+            System.out.println("Cpf inválido!");
+        }
+
     }
 }
